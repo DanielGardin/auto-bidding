9a5965072e379f04adf066821ba20e3358bd70ff
CaioRhoden <caiorhoden@gmail.com>
2024-09-03 20:46:06 -0300

    changes dt + kfold test 5

---
+++ deploy.sh
@@ -0,0 +1,3 @@
+docker build -t registry-intl.cn-beijing.aliyuncs.com/bidding-genai-2024/test:duqiao_td3_4000 -f ./Dockerfile .
+docker push registry-intl.cn-beijing.aliyuncs.com/bidding-genai-2024/test:duqiao_td3_4000
+
