c62adc782af47a00850401a1d6696410378bc06c
CaioRhoden <caiorhoden@gmail.com>
2024-09-12 14:25:24 -0300

    change deploy dir

---
+++ deploy.sh
@@ -0,0 +1,3 @@
+docker build -t registry-intl.cn-beijing.aliyuncs.com/bidding-genai-2024/submit-bidding-2:duqiao_td3_4000 -f ./Dockerfile .
+docker push registry-intl.cn-beijing.aliyuncs.com/bidding-genai-2024/submit-bidding-2:duqiao_td3_4000
+
