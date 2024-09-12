5a88c2f01d2dd5473d6cdc1558d4d925d4edca33
CaioRhoden <caiorhoden@gmail.com>
2024-09-12 16:21:39 -0300

    add one more path to alicloud

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,6 @@
+# docker build -t registry-intl.cn-beijing.aliyuncs.com/bidding-genai-2024/test:duqiao_td3_4000 -f ./Dockerfile .
+# docker push registry-intl.cn-beijing.aliyuncs.com/bidding-genai-2024/test:duqiao_td3_4000
+
 docker build -t registry-intl.cn-beijing.aliyuncs.com/bidding-genai-2024/submit-bidding-2:duqiao_td3_4000 -f ./Dockerfile .
 docker push registry-intl.cn-beijing.aliyuncs.com/bidding-genai-2024/submit-bidding-2:duqiao_td3_4000
 
