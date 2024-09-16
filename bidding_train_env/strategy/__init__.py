04bce878e81a0fbc005d7eacdd6b6cec817e0951
Daniel Gardin <gardin.dan@gmail.com>
2024-09-16 17:54:07 -0300

    PlayerBiddingStrategy now is exported on __init__.py

--- bidding_train_env/strategy/__init__.py
+++ bidding_train_env/strategy/__init__.py
@@ -1,41 +1,49 @@
-from typing import Optional, Protocol
-
-from pathlib import Path
-
-import torch
+from omegaconf import OmegaConf
+from torch import load
 
 from .base_bidding_strategy import BaseBiddingStrategy as BaseBiddingStrategy
-from .onlinelp_bidding_strategy import OnlineLpBiddingStrategy as OnlineLpBiddingStrategy
 from .simple_strategy import SimpleBiddingStrategy as SimpleBiddingStrategy
-# from .player_bidding_strategy import PlayerBiddingStrategy as PlayerBiddingStrategy
 
-from ..utils import get_root_path
-from ..agents.actor import ContinuousDeterminisitcMLP, NormalStochasticMLP
+from ..utils import get_root_path, turn_off_grad
+from ..agents import actor
 
-# Deploying strategy under PlayerBiddingStrategy
-
-deploying_strategy = SimpleBiddingStrategy
-actor              = ContinuousDeterminisitcMLP
-parameters_path    = get_root_path() / "checkpoints/iql_2024-09-11-12:08:25/actor_checkpoint_12.pth"
+# Moved from `import_utils.py` due to circular import
+def get_actor(actor_name: str, **kwargs) -> actor.Actor:
+    return getattr(actor, actor_name)(**kwargs)
 
 
-actor_params       = {
-    'input_dim' : 16,
-    'output_dim': 1,
-    'hidden_dims': [256, 256],
-    'activation': 'relu'
-}
+experiment_name = "dt"
 
-# class PlayerBiddingStrategy(SimpleBiddingStrategy):
-#     def __init__(
-#             self,
-#             budget=100,
-#             name="SimpleStrategy",
-#             cpa=2,
-#             category=1,
-#         ):
+config_path = get_root_path() / f'saved_models/{experiment_name}/config.yaml'
+strategy    = SimpleBiddingStrategy
 
-#         actor = ContinousStochasticMLP(**actor_params)
-#         actor.load_state_dict(torch.load(parameters_path, map_location='cpu'))
+config = OmegaConf.load(config_path)
 
-#         super().__init__(actor, budget, name, cpa, category)
+class PlayerBiddingStrategy(strategy):
+    """
+    Simple Strategy example for bidding.
+    """
+    def __init__(
+            self,
+            budget: float = 100.,
+            name: str     = "PlayerStrategy",
+            cpa: float    = 2,
+            category: int = 1
+        ):
+
+        agent = get_actor(config.model.actor, **config.model.actor_params)
+
+        model_path = get_root_path() / config.saved_models.actor
+
+        agent.load_state_dict(load(model_path))
+
+        turn_off_grad(agent)
+
+
+        super().__init__(
+            agent,
+            budget,
+            name,
+            cpa,
+            category
+        )
