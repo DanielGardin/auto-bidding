c7771e115be780c1aee6c8604750f3f0eacc3bea
Giovani Valdrighi <giovani.valdrighi@gmail.com>
2024-10-11 13:05:52 +0000

    feat: add multi thread to evaluate

--- run/evaluate.py
+++ run/evaluate.py
@@ -2,16 +2,19 @@ from typing import Optional
 
 from pathlib import Path
 from time import perf_counter
+from pathos.multiprocessing import ProcessingPool as Pool
 
 import logging
 import numpy as np
 import pandas as pd
 from torch import load
+import torch
 
 from bidding_train_env.import_utils import get_env, get_strategy, get_actor
 from bidding_train_env.utils import get_root_path, turn_off_grad, set_seed
 from bidding_train_env.envs import BiddingEnv
 from bidding_train_env.strategy import BaseBiddingStrategy
+from bidding_train_env.agents import Actor
 
 from omegaconf import OmegaConf
 
@@ -23,23 +26,26 @@ logger = logging.getLogger(__name__)
 
 
 def evaluate_strategy_offline(
-        env: BiddingEnv,
-        strategy: BaseBiddingStrategy,
-        period: int = 7
+        env      : BiddingEnv,
+        strategy : BaseBiddingStrategy,
+        verbose  : bool = True
     ):
     """
     offline evaluation
     """
+
     start = perf_counter()
 
-    logger.info(
-        f"Evaluating strategy {strategy} on environment {env}."
-    )
+    if verbose:
+        logger.info(
+            f"Evaluating strategy {strategy} on environment {env}."
+        )
 
     cumulative_reward = 0
     num_steps = 0
 
-    env.set_period(period)
+    env.strategy = strategy
+    env.reset()
     obs, info = env.reset()
 
     done = False
@@ -61,13 +67,14 @@ def evaluate_strategy_offline(
     else:
         time_str = f"{total_time//60} min {int(total_time)%60} seconds"
 
-    logger.info(f"Strategy evaluation completed within {num_steps} steps, in {time_str}.")
-    logger.info(f"Total reward: {cumulative_reward}")
-    logger.info(f"Total cost: {strategy.budget - env.remaining_budget}")
-    logger.info(f"Total conversions: {info['conversions']}")
-    logger.info(f"Total wins: {info['wins']}")
-    logger.info(f"CPA: {info['cpa']}")
-    logger.info(f"Score: {info['score']}")
+    if verbose:
+        logger.info(f"Strategy evaluation completed within {num_steps} steps, in {time_str}.")
+        logger.info(f"Total reward: {cumulative_reward}")
+        logger.info(f"Total cost: {strategy.budget - env.remaining_budget}")
+        logger.info(f"Total conversions: {info['conversions']}")
+        logger.info(f"Total wins: {info['wins']}")
+        logger.info(f"CPA: {info['cpa']}")
+        logger.info(f"Score: {info['score']}")
 
     return {
         "cumulative_reward": cumulative_reward,
@@ -93,7 +100,12 @@ if __name__ == "__main__":
         type=int,
         default=1,
     )
-
+    parser.add_argument(
+        "--n_jobs",
+        type=int,
+        default=1,
+    )
+    device = "cpu"
     args = parser.parse_args()
 
     config = OmegaConf.load(args.config)
@@ -101,11 +113,11 @@ if __name__ == "__main__":
     set_seed(config.general.seed)
 
     actor = get_actor(config.model.actor, **config.model.actor_params)
-    actor = actor.to(config.general.device)
+    actor = actor.to(device)
 
     model_path = get_root_path() / config.saved_models.actor
 
-    state_dict = load(model_path, map_location=config.general.device)
+    state_dict = load(model_path, map_location=device)
     actor.load_state_dict(state_dict)
 
     turn_off_grad(actor)
@@ -129,29 +141,43 @@ if __name__ == "__main__":
 
         evaluate_strategy_offline(env, strategy, period=7)
     else:
-        advertiser_data = pd.read_csv(get_root_path() / "data/traffic/efficient_repr/advertiser_data.csv")
+        torch.set_num_threads(1)
+        advertiser_data = pd.concat([
+            pd.read_csv(get_root_path() / f"data/traffic/{f}/advertiser_data.csv")
+            for f in ["efficient_repr", "new_efficient_repr"]
+        ])
         budget = advertiser_data.sample(args.n_tests)["budget"].values
         cpa = advertiser_data.sample(args.n_tests)["CPAConstraint"].values
         category = advertiser_data.sample(args.n_tests)["advertiserCategoryIndex"].values
+        period = np.random.choice([24, 25, 26], args.n_tests)
         results = []
-        for i in range(args.n_tests):
 
+        aux = []
+        for i in range(args.n_tests):
             strategy = get_strategy(
                 config.model.strategy,
                 actor,
-                budget[i],
-                cpa[i],
-                category[i]
+                budget[i].item(),
+                cpa[i].item(),
+                category[i].item()
             )
+            aux.append(strategy)
 
-            period = np.random.choice([24, 25, 26])
-            env = get_env(
-                config.environment.environment,
-                strategy,
-                period=period,
-                **config.environment.environment_params
+        env = get_env(
+            "OfflineBiddingEnv",
+            strategy = strategy,
+            period = period[0].item(),
+        )
+
+        evaluate_strategy_offline_ = lambda strategy: evaluate_strategy_offline(env, strategy, False)
+        with Pool(args.n_jobs) as p:
+            results = p.map(
+                evaluate_strategy_offline_,
+                aux
             )
-            results.append(evaluate_strategy_offline(env, strategy, period=period))
+           
         results = pd.DataFrame(results)
 
-        print(results.mean())
+        # replace cpa inf with 50000
+        results["cpa"] = results["cpa"].replace(np.inf, 50000)
+        print(results.describe())
