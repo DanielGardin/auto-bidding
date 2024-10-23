443e5c8bf0db8c02739afb50e477db7332b8dfe4
Giovani Valdrighi <giovani.valdrighi@gmail.com>
2024-10-23 14:25:33 +0000

    fix: separated deploy bash

--- rl_deploy.sh
+++ rl_deploy.sh
@@ -1,4 +1,2 @@
 docker build -t registry-intl.cn-beijing.aliyuncs.com/nips_mrai/nipsenv:duqiao_test_td3 -f ./Dockerfile_submit .
 docker push registry-intl.cn-beijing.aliyuncs.com/nips_mrai/nipsenv:duqiao_test_td3
-
-ls
